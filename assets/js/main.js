f32b66845a68a61736129bd26e84d461ed6c5e6c
thedreamJK7 <jkubaev7@gmail.com>
2021-10-21 02:15:23 +0500

    toglle

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,26 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
-
+const navOpen = document.getElementById('nav-open');
+const navMenu = document.getElementById('nav-menu');
+const navBg = document.getElementById('nav-bg');
+const navClose = document.getElementById('nav-close')
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-
+if(navOpen) {
+    navOpen.addEventListener('click', ()=> {
+        navMenu.classList.add('show_menu');
+        navBg.classList.add('show_menu');
+    })
+}
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-
+if(navClose) {
+    navClose.addEventListener('click', ()=> {
+        navMenu.classList.remove('show_menu');
+        navBg.classList.remove('show_menu');
+    })
+}
 
 /*==================== REMOVE MENU MOBILE ====================*/
 
